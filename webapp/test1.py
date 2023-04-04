065eee482cc646f726fafd4fdfd3579c93fa6da0
Simo680 <erik.simonyan2005@gmail.com>
2023-04-04 13:03:35 +0300

    first commit

---
+++ webapp/test1.py
@@ -0,0 +1,7 @@
+todos = open('todos.txt', 'a')
+
+print('Put out the trash.', file=todos)
+print('Feed the cat.', file=todos)
+print('Prepare tax return.', file=todos)
+
+todos.close()
---
+++ webapp/test2.py
@@ -0,0 +1,10 @@
+tasks = open('todos.txt')
+
+for chore in tasks:
+    print(chore)
+tasks.close()
+
+with open('todos.txt') as tasks:
+    for chore in tasks:
+        print(chore, end='')
+
